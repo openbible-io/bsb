4fe94dc8e31006f4edfcc1df7d545022a4442eca
thesmartwon <thesmartwon@pm.me>
2024-09-17 16:47:19 -0400

    simpler

--- ci.sh
+++ ci.sh
@@ -1,26 +1,24 @@
-patch() {
-	BUMPED=$(git describe --tags --abbrev=0 | awk -F. -v OFS=. '{$NF += 1 ; print}')
-	git tag $BUMPED
-	git push --tags origin master
-	npm version --no-git-tag-version $BUMPED
-	npm publish --provenance --access public
-}
-
 npm ci
 if [[ "$GITHUB_EVENT_NAME" != "push" ]]; then
 	./pull.sh
 fi
 node build.js
 
-git config user.name ci
-git config user.email ci@openbible.io
-if [[ "$GITHUB_EVENT_NAME" != "push" ]]; then
+if [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
+	# Do a release
+	BUMPED=$(git describe --tags --abbrev=0 | awk -F. -v OFS=. '{$NF += 1 ; print}')
+	git tag $BUMPED
+	git push --tags origin master
+	npm version --no-git-tag-version $BUMPED
+	npm publish --provenance --access public
+else
+	# Push an update for later review + release.
 	git status -s
 	if [[ -n "$(git status -s)" ]]; then
 		git add .
+		git config user.name ci
+		git config user.email ci@openbible.io
 		git commit -m  "Pull new version"
-		patch
+		git push
 	fi
-else
-	patch
 fi
