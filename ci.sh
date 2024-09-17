8395f733157ad44ebbdc6fdaed498ffedb721da7
thesmartwon <thesmartwon@pm.me>
2024-09-17 16:51:41 -0400

    fetch tags before bump

--- ci.sh
+++ ci.sh
@@ -6,6 +6,7 @@ node build.js
 
 if [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
 	# Do a release
+	git fetch --tags
 	BUMPED=$(git describe --tags --abbrev=0 | awk -F. -v OFS=. '{$NF += 1 ; print}')
 	git tag $BUMPED
 	git push --tags origin master
