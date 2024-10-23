6c20e5e4493d2e554111145c0abf7c0b244fc5ed
thesmartwon <thesmartwon@pm.me>
2024-10-23 16:42:20 -0400

    bump inside pull

--- build/pull-audio.sh
+++ build/pull-audio.sh
@@ -1,17 +1,26 @@
 #!/bin/env bash
-# Pulls audio if apache web server has files dated after latest `audio-YYYY-mm-dd` release tag.
-# If so, will create new release with ALL audio.
+# Pulls audio if apache web server has files dated after latest `audio-$version` release tag.
+#
+# Ideally these files would be versioned in Git LFS and updated separately, but Github only allows
+# a total of 1GB of LFS for free. Because of that, we'll make a Github release.
 mkdir -p dist
 cd dist
 
 # Public Github runners have a disk limit of 14GB.
 for v in souer; do
-	TAG="audio-$v-$(date '+%Y-%m-%d')"
-	LAST_DOWNLOAD=$(gh release view $TAG --json 'tagName' -q '.tagName' | grep -oP '\d{4}-\d{2}-\d{2}')
+	LAST_RELEASE=$(gh release ls --json tagName,createdAt -q "map(select(.tagName | test(\"^audio-$v-.*\"))) | .[]")
+	LAST_TAG=$(echo $LAST_RELEASE | jq ".tagName")
+	LAST_RELEASE_DATE=$(echo $LAST_RELEASE | jq ".createdAt" | cut -d-10)
+
 	set -e
 	if deno task audio --since "$LAST_DOWNLOAD" $v; then
-		# Github has a 2GB file limit
+		# 0 = no compression (mp3 is already compressed, at most 1-2% gains from DEFLATE)
+		# r = recursive
+		# m = move into zipfile
+		# s 1950m = split into 1950M since Github has a 2GB file limit
 		zip -0rms 1950m $v.zip *
+		ls
+		VERSION=$(echo ${LAST_TAG:="audio-$v-v0.0.0"} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 		gh release create --latest=false --title "$v Audio" $TAG *
 		rm -rf *
 	else
