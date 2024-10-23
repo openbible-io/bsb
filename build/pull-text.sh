f91000a1164e34397403f41ef573d9f362e3074d
thesmartwon <thesmartwon@pm.me>
2024-10-23 12:48:29 -0400

    overwrite files

--- build/pull-text.sh
+++ build/pull-text.sh
@@ -2,5 +2,5 @@
 # Pulls text to `bsb` dir.
 curl https://bereanbible.com/bsb_tables.xlsx > bsb/bsb_tables.xlsx
 curl https://bereanbible.com/bsb_usfm.zip > bsb_usfm.zip
-7z x bsb_usfm.zip -obsb
+7z x -aoa bsb_usfm.zip -obsb
 rm bsb_usfm.zip
