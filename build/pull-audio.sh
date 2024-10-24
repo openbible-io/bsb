7c2dd7cd1950070b0e28482869d0f6bea1cda939
thesmartwon <thesmartwon@pm.me>
2024-10-23 21:47:53 -0400

    use matrix

--- build/pull-audio.sh
+++ build/pull-audio.sh
@@ -6,30 +6,29 @@
 mkdir -p dist
 cd dist
 
-# Public Github runners have a disk limit of 14GB.
-for v in souer gilbert hays; do
-	LAST_RELEASE=$(gh release ls --json tagName,createdAt -q "map(select(.tagName | test(\"^audio-$v-.*\"))) | .[]")
-	LAST_TAG=$(echo $LAST_RELEASE | jq -r ".tagName")
-	LAST_RELEASE_DATE=$(echo $LAST_RELEASE | jq -r ".createdAt" | cut -c-10)
-	if [ $1 = 'true' ]; then
-		echo "good"
-		LAST_RELEASE_DATE=""
-	fi
+v=$1
 
-	set -e
-	if deno task audio --since "$LAST_RELEASE_DATE" $v; then
-		find . -name '*.mp3' | parallel ffmpeg -i {} {.}.webm
-		find . -name '*.mp3' -exec rm {} \;
-		# 0 = no compression (webm is already compressed, at most 1-2% gains from DEFLATE)
-		# r = recursive
-		# m = move into zipfile
-		# s 1950m = split into 1950M since Github has a 2GB file limit
-		zip -0rms 1950m $v.zip *
-		ls -lah
-		TAG=$(echo ${LAST_TAG:="audio-$v-v0.0.0"} | awk -F. -v OFS=. '{$NF += 1 ; print}')
-		gh release create --latest=false --title "$v audio" $TAG *
-		rm -rf *
-	else
-		echo 'no changes'
-	fi
-done
+# Public Github runners have a disk limit of 14GB.
+LAST_RELEASE=$(gh release ls --json tagName,createdAt -q "map(select(.tagName | test(\"^audio-$v-.*\"))) | .[]")
+LAST_TAG=$(echo $LAST_RELEASE | jq -r ".tagName")
+LAST_RELEASE_DATE=$(echo $LAST_RELEASE | jq -r ".createdAt" | cut -c-10)
+if [ "$2" = 'true' ]; then
+	echo "forcing redownload"
+	LAST_RELEASE_DATE=""
+fi
+
+set -e
+if deno task audio --since "$LAST_RELEASE_DATE" $v; then
+	find . -name '*.mp3' | parallel 'ffmpeg -i {} {.}.webm; rm {}'
+	# 0 = no compression (webm is already compressed, at most 1-2% gains from DEFLATE)
+	# r = recursive
+	# m = move into zipfile
+	# s 1950m = split into 1950M since Github has a 2GB file limit
+	zip -0rms 1950m $v.zip *
+	ls -lah
+	TAG=$(echo ${LAST_TAG:="audio-$v-v0.0.0"} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+	gh release create --latest=false --title "$v audio" $TAG *
+	rm -rf *
+else
+	echo 'no changes'
+fi
